e236b26f94d24abef543370a5200fe006a13b526
Drake Damon <dddamon06@gmail.com>
2025-02-19 20:20:23 -0500

    completed lab

--- server/models.py
+++ server/models.py
@@ -5,9 +5,9 @@ db = SQLAlchemy()
 
 class Movie(db.Model, SerializerMixin):
     __tablename__ = 'movies'
-
+    
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
-
+    
     def __repr__(self):
         return f'<Movie {self.title}>'
